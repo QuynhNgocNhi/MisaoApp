127611756151aa18a086c11ec9b1927df804099a
Nhi Huỳnh Thị Yến <19521962@ms.uit.edu.vn>
2022-05-31 22:57:09 +0700

    okk

--- src/screen/ChatScreen/UserItem.tsx
+++ src/screen/ChatScreen/UserItem.tsx
@@ -62,19 +62,11 @@ const userItem = ({ chatRoom }: any) => {
 
                 </View>
             </View>
-<<<<<<< HEAD
-            {/* {user?.id === chatRoom?.buyer?.id && chatRoom?.seller?.profile_image && chatRoom?.buyer?.profile_image && (
-                <TouchableOpacity style={styles.productImageContainer} onPress={() => navigation.navigate('ChatRoomScreen', { id: chatRoom?.id })} >
-                    <Image style={styles.productImage}
-                        source={{ uri: user?.id === chatRoom?.buyer?.id ? chatRoom?.seller?.profile_image_url : chatRoom?.buyer?.profile_image_url }} />
-                </TouchableOpacity>
-=======
             {/* {user?.id === chatRoom?.buyer?.id ? chatRoom?.seller?.profile_image : chatRoom?.buyer?.profile_image && (
                 <View style={styles.productImageContainer}>
 
                     <Image onPress={() => navigation.navigate('ChatRoomScreen', { id: chatRoom.id })} style={styles.productImage} source={{ uri: user?.id === chatRoom?.buyer?.id ? chatRoom?.seller?.profile_image_url : chatRoom?.buyer?.profile_image_url }} />
                 </View>
->>>>>>> 5ee0a9af064ec9660f669dd0edc464a71abec4cd
             )} */}
 
 
